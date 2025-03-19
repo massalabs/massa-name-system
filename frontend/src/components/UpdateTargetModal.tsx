9303c5d2c7255014fd918ef1c90aa4f0a76ab7dd
Peterjah <ps@massa.net>
2025-03-19 10:51:02 +0100

    add renounce ownership

--- frontend/src/components/UpdateTargetModal.tsx
+++ frontend/src/components/UpdateTargetModal.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
 import { useMnsManagement } from '../hooks/useMnsManagement';
-import { ChangeModal } from './UpdateModal';
+import { DnsUserEntryListResult } from '../utils/interface';
+import {
+  Button,
+  Input,
+  PopupModal,
+  PopupModalContent,
+  PopupModalHeader,
+} from '@massalabs/react-ui-kit';
 
 type UpdateTargetModalProps = {
   isOpen: boolean;
-  domain: string;
+  mns: DnsUserEntryListResult;
   close: () => void;
 };
 
 export function UpdateTargetModal({
   isOpen,
-  domain,
+  mns,
   close,
 }: UpdateTargetModalProps) {
   const [newTargetAddress, setNewTargetAddress] = useState<string>('');
@@ -20,17 +27,34 @@ export function UpdateTargetModal({
   if (!isOpen) return null;
 
   const onSave = async () => {
-    changeTargetAddressDnsEntry(domain, newTargetAddress);
+    changeTargetAddressDnsEntry(mns.domain, newTargetAddress);
     close();
   };
 
+  const title = `Change target address of ${mns.domain}.massa`;
+
   return (
-    <ChangeModal
-      title={`Change target address of ${domain}.massa`}
-      inputPlaceholder="Enter a new target address"
-      setInputValue={setNewTargetAddress}
-      onSave={onSave}
-      onClose={close}
-    />
+    <div className="fixed inset-0 flex items-center justify-center">
+      <PopupModal onClose={close}>
+        <PopupModalHeader customClassHeader="mb-8">
+          <h2 className="mas-h2">{title}</h2>
+        </PopupModalHeader>
+        <PopupModalContent>
+          <p className="mas-body pb-2">Enter Address</p>
+          <Input
+            customClass="w-96 border-none mb-8"
+            placeholder={mns.targetAddress}
+            onChange={(e) => setNewTargetAddress(e.target.value)}
+          />
+          <div className="flex flex-row-reverse pb-12">
+            <div className="w-32">
+              <Button onClick={onSave}>
+                <div>Save</div>
+              </Button>
+            </div>
+          </div>
+        </PopupModalContent>
+      </PopupModal>
+    </div>
   );
 }
